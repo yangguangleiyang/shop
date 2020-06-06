f6d7006058d53661012ebadb1580d9e8fd8abf16
yangguangleiyang <yangguangleiyang123456789>
2020-06-06 18:11:33 +0800

    Merge branch 'master' of https://github.com/yangguangleiyang/shop
     Conflicts:
    	hello world.py

--- hello world.py
+++ hello world.py
@@ -1,3 +1,11 @@
-print("hello world")
-print("123")
-print("新世界的学生")
+#print("hello world")
+
+'''age_of_princal = 56
+guess_age = int(input("your age:"))
+if guess_age == age_of_princal:
+    print("yes")
+else:
+    print("it's wrong")'''
+
+
+
