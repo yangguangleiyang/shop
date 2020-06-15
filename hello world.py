0ebad17b20070333c809962c6f4493a41c9fba6c
yangguangleiyang <yangguangleiyang123456789>
2020-06-15 09:13:17 +0800

    新增while循环

--- hello world.py
+++ hello world.py
@@ -5,7 +5,45 @@ guess_age = int(input("your age:"))
 if guess_age == age_of_princal:
     print("yes")
 else:
-    print("it's wrong")'''
+    print("it's wrong")
+#----------------------------------
+age = 50
+flag = True #True和False必须大写
+while flag:
+    user_age = int(input("请输入您的年龄："))
+    if user_age == age:
+        print("yes")
+        flag = False
+    elif user_age > age:
+        print("it is bigger")
+    elif user_age < age:
+        print("it is smaller")
+print("end")
+#也可以这样写： 使用break
+age = 50
+
+while True:
+    user_age = int(input("请输入您的年龄："))
+    if user_age == age:
+        print("yes")
+        break
+    elif user_age > age:
+        print("it is bigger")
+    elif user_age < age:
+        print("it is smaller")
+print("end")
 
 
+num = 1
+while num < 100:
+    if num%2 == 1:
+        print(num)
+    num += 1
+'''
 
+num = 1
+while num < 10:
+    num += 1
+    if num == 3:
+        continue
+    print(num)
